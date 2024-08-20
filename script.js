b355f9b1365da2240d3e6ec0ed88f2553928b24a
vivek kuamar <vivekkumar474849@gmail.com>
2024-08-20 16:31:02 +0530

    this is my git learning assignment

---
+++ script.js
@@ -0,0 +1,69 @@
+// this is git lesson where we learn how to work git version control
+
+
+
+// Task 1
+// Initialize a Git repository for your JavaScript project.
+
+// 1.open terminal and there is some command to initialize the git reposatory
+// 2.git init
+// 3.git add .
+// 4.git commit -m "this is my git assignment 7"
+// 5.git remote add origin "link of reposatory"
+// 6.git push -u origin main
+
+
+
+// task 2.
+// Create a file named script.js with a simple JavaScript function.
+
+// To create file named script.js use touch script.js
+
+const gitfunc=(a,b)=>{
+         return a+b;
+}
+gitfunc(5,6)
+
+
+
+
+
+
+// Task-3:
+// Add and commit the changes.
+
+// use these command to add and commit
+// git add.
+// git commit -m "this is assignment 7"
+
+
+// 2
+// Task-1:
+// Create a new branch named feature-greeting.
+// command to check current branch use ->git branch
+// * main
+
+// command to create new branch feature-greeting
+//  ->git branch feature-greeting
+
+//  command to check all branch->git branch
+//   feature-greeting
+// * main
+
+// * indicating current branch
+
+// command to change branch main to feature-greeting
+
+// git checkout feature-greeting
+// //Switched to branch 'feature-greeting'
+
+
+// command to check my current branch
+// git branch
+// * feature-greeting
+//   main
+
+// * indicating this is current branch
+
+
+
